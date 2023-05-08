4e9092eeabd84453179ad3a30b46200760e99376
Paul Welsh <spacedawwwg@gmail.com>
2023-05-08 11:50:36 +0100

    fix: submission date

--- src/views/HomePage/HomePageInformation.vue
+++ src/views/HomePage/HomePageInformation.vue
@@ -11,7 +11,7 @@
       <li>
         Lists are to be submitted via email to
         <a href="mailto:northernkings.kow@gmail.com">northernkings.kow@gmail.com</a>
-        by Midnight, Sunday 13th May. Players who submit their list on time will receive a +5
+        by Midnight, Sunday 8th May. Players who submit their list on time will receive a +5
         Tournament Point bonus.
       </li>
       <li>
